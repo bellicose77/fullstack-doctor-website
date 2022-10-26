e370adeb3f1044fb122e869b67f76730eba28e01
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 12:41:11 +0600

    navigation done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Home from './Pages/Home/Home'
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    
+    </Routes>
     </div>
   );
 }
---
+++ frontend/src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h2>This is home page</h2>
+        </div>
+    );
+};
+
+export default Home;
