b17543ef2228c42b6656596bc09316d19a2bd57b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 17:57:12 +0600

    props function done

--- frontend/src/components/Service/Service.js
+++ frontend/src/components/Service/Service.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {title,desc,img} = service;
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
-    <img src="https://placeimg.com/400/225/arch" alt="Shoes" class="rounded-xl" />
+    <img src={img} alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">Shoes!</h2>
