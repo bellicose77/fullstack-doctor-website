529cf5a22774ecf34e376e3e61097db237861b75
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 17:44:41 +0600

    service section

--- frontend/src/components/Services/Services.js
+++ frontend/src/components/Services/Services.js
@@ -7,6 +7,9 @@ const Services = () => {
                 <h3 className='text-primary text-xl font-bold uppercase'>OUR SERVICES</h3>
                 <h2 className='text-4xl'>Services We Provide</h2>
             </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
