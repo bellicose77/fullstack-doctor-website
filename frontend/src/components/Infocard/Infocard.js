ad63f29a4025889614f6944213be74bdeb1e6730
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 16:39:15 +0600

    card done

--- frontend/src/components/Infocard/Infocard.js
+++ frontend/src/components/Infocard/Infocard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Infocard = ({img}) => {
+const Infocard = ({img,cardtitle,bgClass}) => {
     return (
-    <div class="card card-side bg-base-100 shadow-xl bg-accent">
-     <figure><img src={img} alt="Movie"/></figure>
+    <div class={`card card-side bg-base-100 shadow-xl ${bgClass}`}>
+     <figure className='pl-5'><img src={img} alt="Movie"/></figure>
     <div class="card-body text-white">
-    <h2 class="card-title">New movie is released!</h2>
+    <h2 class="card-title">{cardtitle}</h2>
     <p>Click the button to watch on Jetflix app.</p>
   </div>
 </div>
