50c1bc468441bc92f86531bd3696a6144a2568c5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 16:29:29 +0600

    dynamic content

--- frontend/src/components/Info/Info.js
+++ frontend/src/components/Info/Info.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Infocard from '../Infocard/Infocard';
-import img from '../../assets/icons/clock.svg'
+import clock from '../../assets/icons/clock.svg'
+import marker from '../../assets/icons/marker.svg'
+import phone from '../../assets/icons/phone.svg'
 
 const Info = () => {
     return (
         <div class="grid grid-cols-3 gap-5">
-            <Infocard img={img} cardtitle={Opening}/>
-            <Infocard img={img} cardtitle={location}/>
-            <Infocard img={img} cardtitle={Contact}/>
+            <Infocard img={clock} cardtitle={"Opening hour"}/>
+            <Infocard img={marker} cardtitle={"visit our Location"}/>
+            <Infocard img={phone} cardtitle={"Contact"}/>
         </div>
     );
 };
