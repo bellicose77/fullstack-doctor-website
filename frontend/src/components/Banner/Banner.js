1f4f01a05255b5e680e2200ced9dd12001f6b4ec
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 16:00:33 +0600

    info card

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
         <div class="hero min-h-screen">
           <div class="hero-content flex-col lg:flex-row-reverse">
-           <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+           <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 class="text-5xl font-bold">Your New Smile Starts</h1>
+      <p class="py-6"> lorem10
+      </p>
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
