364e1444df944ed9c8129f45ccf8e74f0e6776d8
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 17:33:52 +0600

    service section

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Info from '../../components/Info/Info';
 import Navbar from '../../components/Navbar/Navbar';
+import Services from '../../components/Services/Services';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar/>
             <Banner/>
             <Info/>
+            <Services/>
         </div>
     );
 };
