95a34d339a30d66356bd492b601e9637f9ffb1da
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-26 17:53:40 +0600

    fake data for service

--- frontend/src/components/Services/Services.js
+++ frontend/src/components/Services/Services.js
@@ -1,6 +1,30 @@
 import React from 'react';
+import floried from '../../assets/images/fluoride.png'
+import cavity from '../../assets/images/cavity.png'
+import teeth from '../../assets/images/whitening.png'
 
 const Services = () => {
+    const services = [
+        {
+            _id:1,
+            title:'Floried treatment',
+            desc:"There is big discount",
+            img:floried
+        },
+        {
+            _id:2,
+            title:'Cavitiy filling',
+            desc:"do ti quicly",
+            img:cavity
+
+        },
+        {
+            _id:3,
+            title:'Teeth Whiting',
+            desc:"got a surprise",
+            img:teeth
+        }
+    ]
     return (
         <div className='my-28'>
             <div className='text-center'>
@@ -8,7 +32,7 @@ const Services = () => {
                 <h2 className='text-4xl'>Services We Provide</h2>
             </div>
             <div>
-                
+
             </div>
         </div>
     );
