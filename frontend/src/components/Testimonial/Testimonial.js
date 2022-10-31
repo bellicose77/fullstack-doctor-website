985469871556f89c71563e26834f68473563c7d9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 12:46:49 +0600

    import all photo

--- frontend/src/components/Testimonial/Testimonial.js
+++ frontend/src/components/Testimonial/Testimonial.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import quote from '../../assets/icons/quote.svg'
+import people1 from '../../assets/images/people1.png'
+import people2 from '../../assets/images/people2.png'
+import people3 from '../../assets/images/people3.png'
 
 const Testimonial = () => {
+    const reviews = [
+        {
+            _id:1,
+            name:"Windos henrry",
+            desc:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location:'London',
+            img:
+
+        }
+    ]
     return (
         <section className='mt-28'>
             <div className='flex justify-between'>
@@ -16,7 +29,7 @@ const Testimonial = () => {
                 </div>
 
             </div>
-            
+
             <div>
 
             </div>
