73d3716d58b142646f8b8f27c1b769dd258fe68a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 12:28:25 +0600

    testimonial component

--- frontend/src/components/Testimonial/Testimonial.js
+++ frontend/src/components/Testimonial/Testimonial.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import quote from '../../assets/icons/quote.svg'
 
 const Testimonial = () => {
     return (
         <section>
-            <div>
+            <div className='flex justify-between'>
                 <div>
                     <h4 className='text-xl text-primary font-bold'>Testimonial</h4>
-                    <h2>What our patients says</h2>
+                    <h2 className='text-3xl'>What our patients says</h2>
 
                 </div>
                 <div>
+                    <img src={quote}/>
 
                 </div>
 
