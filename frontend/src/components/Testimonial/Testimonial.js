1ea2a2f6636f6db9f95c9fde8364ac7f9563b96a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 12:47:47 +0600

    fake data done

--- frontend/src/components/Testimonial/Testimonial.js
+++ frontend/src/components/Testimonial/Testimonial.js
@@ -11,7 +11,23 @@ const Testimonial = () => {
             name:"Windos henrry",
             desc:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
             location:'London',
-            img:
+            img:people1
+
+        },
+        {
+            _id:2,
+            name:"Windos henrry",
+            desc:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location:'London',
+            img:people2
+
+        },
+        {
+            _id:3,
+            name:"Windos henrry",
+            desc:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            location:'London',
+            img:people3
 
         }
     ]
