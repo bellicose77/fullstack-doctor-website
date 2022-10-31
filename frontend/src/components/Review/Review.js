7da67e3563612e28373873c0e13e8887ac77e800
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 13:02:16 +0600

    testimonial done

--- frontend/src/components/Review/Review.js
+++ frontend/src/components/Review/Review.js
@@ -6,9 +6,17 @@ const Review = ({review}) => {
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
             <div class="card-body">
                  <p>{desc}</p>
-              <div class="card-actions justify-end">
-                
-             </div>
+              <div class="flex items-center">
+                  <div class="avatar">
+                     <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+                         <img src={img}/>
+                     </div>
+                  </div>
+                <div>
+                    <h3>{name}</h3>
+                    <h4>{location}</h4>
+                </div>
+              </div>
         </div>
      </div>
     );
