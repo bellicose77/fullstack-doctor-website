f4c79814d9060503750fa94004c00fcc66a424ee
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 12:54:24 +0600

    send props to review component

--- frontend/src/components/Testimonial/Testimonial.js
+++ frontend/src/components/Testimonial/Testimonial.js
@@ -3,6 +3,7 @@ import quote from '../../assets/icons/quote.svg'
 import people1 from '../../assets/images/people1.png'
 import people2 from '../../assets/images/people2.png'
 import people3 from '../../assets/images/people3.png'
+import Review from '../Review/Review';
 
 const Testimonial = () => {
     const reviews = [
@@ -46,8 +47,11 @@ const Testimonial = () => {
 
             </div>
 
-            <div>
-
+            <div className='grid grid-cols-3 gap-5'>
+              {
+                reviews.map(review=> <Review
+                   key={review._id} review={review}></Review>)
+              }
             </div>
         </section>
     );
