cbfbdedfbe02cbe5560d806a31c31fe4a18df31d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-10-31 12:24:59 +0600

    testimonial component

--- frontend/src/components/Testimonial/Testimonial.js
+++ frontend/src/components/Testimonial/Testimonial.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Testimonial = () => {
     return (
         <section>
-            
+            <div>
+                <div>
+                    <h4>Testimonial</h4>
+                    <h2>What our patients says</h2>
+
+                </div>
+                <div>
+
+                </div>
+
+            </div>
+            <div>
+
+            </div>
         </section>
     );
 };
