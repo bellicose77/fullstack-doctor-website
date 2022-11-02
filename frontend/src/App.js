4ba6a7601c9ad709046833e60c739dcceee6bfe5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-02 12:47:22 +0600

    appoinment header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Home from './Pages/Home/Home'
 import { Routes, Route, Link } from "react-router-dom";
+import Appoinment from './Pages/Appoinment/Appoinment';
 
 function App() {
   return (
     <div>
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/appointment' element = {<Appoinment/>}/>
     
     </Routes>
     </div>
--- frontend/src/Pages/Appoinment/Appoinment.js
+++ frontend/src/Pages/Appoinment/Appoinment.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AppointBanner from '../../components/AppointBanner/AppointBanner';
 import Navbar from '../../components/Navbar/Navbar';
 
 const Appoinment = () => {
     return (
         <div>
             <Navbar/>
+            <AppointBanner/>
             
         </div>
     );
