0d0fa5213ddac6dc435e19f151341c96ddd4b72f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-02 12:48:38 +0600

    appoinment header

--- frontend/src/components/AppointBanner/AppointBanner.js
+++ frontend/src/components/AppointBanner/AppointBanner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png'
 
 const AppointBanner = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
-               <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+               <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
                    <h1 class="text-5xl font-bold">Box Office News!</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
