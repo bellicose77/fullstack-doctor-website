4f6d8feeb14439141d06fb3200266d134bf27892
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-02 12:50:16 +0600

    day picker done

--- frontend/src/components/AppointBanner/AppointBanner.js
+++ frontend/src/components/AppointBanner/AppointBanner.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import chair from '../../assets/images/chair.png'
+import chair from '../../assets/images/chair.png';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 const AppointBanner = () => {
     return (
@@ -7,9 +9,7 @@ const AppointBanner = () => {
             <div class="hero-content flex-col lg:flex-row-reverse">
                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                   <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <DayPicker/>
                 </div>
             </div>
         </div>
