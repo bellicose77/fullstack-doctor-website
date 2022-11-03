54191b3ff16ae58d7c462213a9aa222b4f38cf58
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 13:30:16 +0600

    all solts of appointment

---
+++ frontend/src/components/AppointmentSlot/AppointmentSlot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppointmentSlot = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AppointmentSlot;
--- frontend/src/components/AvailAppointment/AvailAppointment.js
+++ frontend/src/components/AvailAppointment/AvailAppointment.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 
 const AvailAppointment = ({date}) => {
-    const [fakeData,setFakeData] = useState([])
+    const [fakeData,setFakeData] = useState([]);
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res=>res.json())
+        .then(data=>setFakeData(data))
+    },[]);
+    console.log(fakeData)
     return (
         <div>
             <h4 className='text-xl text-primary text-center'>Available Appointments on : {format(date, 'PP')}</h4>
