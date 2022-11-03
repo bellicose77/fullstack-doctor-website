ae572bb66c8e7009df7b2ac7c2e51a76fd2cfe6b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 20:00:22 +0600

    input field

--- frontend/src/components/BookModal/BookModal.js
+++ frontend/src/components/BookModal/BookModal.js
@@ -9,7 +9,9 @@ const BookModal = ({treatment}) => {
             <div class="modal-box">
             <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                <h3 class="font-bold text-lg text-secondary">booking name:{name}</h3>
-               <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+               <form>
+               <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+               </form>
                <div class="modal-action">
                
                </div>
