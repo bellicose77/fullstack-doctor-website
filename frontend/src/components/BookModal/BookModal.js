8ffcf397bee1ecd586df7d99a37839c89bec39e7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 19:55:45 +0600

    form of modal

--- frontend/src/components/BookModal/BookModal.js
+++ frontend/src/components/BookModal/BookModal.js
@@ -11,7 +11,7 @@ const BookModal = ({treatment}) => {
                <h3 class="font-bold text-lg text-secondary">booking name:{name}</h3>
                <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
                <div class="modal-action">
-               <label for="booking-modal" class="btn">Yay!</label>
+               
                </div>
             </div>
         </div>
