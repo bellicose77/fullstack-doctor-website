e5db7813398f86f01b7059d869a4f24ce4be6995
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 13:44:50 +0600

    condition rendering

--- frontend/src/components/AppointmentSlot/AppointmentSlot.js
+++ frontend/src/components/AppointmentSlot/AppointmentSlot.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const AppointmentSlot = ({solt}) => {
-    const {name,solts} = solt;
+    const {name,slots} = solt;
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
           <h2 class="card-title">{name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{slots.length} {slots.length ? "spaces":"space"} avaiable</p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
           </div>
