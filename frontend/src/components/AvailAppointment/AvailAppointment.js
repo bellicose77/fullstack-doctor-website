04fbbc72705e56e7574bed5ad6e32f3cb2a6885d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 13:33:33 +0600

    all solts of appointment

--- frontend/src/components/AvailAppointment/AvailAppointment.js
+++ frontend/src/components/AvailAppointment/AvailAppointment.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
+import AppointmentSlot from '../AppointmentSlot/AppointmentSlot';
 
 const AvailAppointment = ({date}) => {
-    const [fakeData,setFakeData] = useState([]);
+    const [fakeDatas,setFakeData] = useState([]);
     useEffect(()=>{
         fetch('services.json')
         .then(res=>res.json())
         .then(data=>setFakeData(data))
     },[]);
-    console.log(fakeData)
+    console.log(fakeDatas)
     return (
         <div>
             <h4 className='text-xl text-primary text-center'>Available Appointments on : {format(date, 'PP')}</h4>
+            <div>
+                {
+                    fakeDatas.map(solt=><AppointmentSlot 
+                        key={solt._id}
+                        solt={solt}
+                    ></AppointmentSlot>)
+                }
+            </div>
         </div>
+        
     );
 };
 
