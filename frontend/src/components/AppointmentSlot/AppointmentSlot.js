1c3e4c508b90a745de19744659690585f5b53d82
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-11-03 13:48:58 +0600

    slots appointment design done

--- frontend/src/components/AppointmentSlot/AppointmentSlot.js
+++ frontend/src/components/AppointmentSlot/AppointmentSlot.js
@@ -4,11 +4,11 @@ const AppointmentSlot = ({solt}) => {
     const {name,slots} = solt;
     return (
         <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-          <h2 class="card-title">{name}</h2>
+        <div class="card-body text-center">
+          <h2 className="card-title text-secondary justify-center">{name}</h2>
           <p>{slots.length} {slots.length ? "spaces":"space"} avaiable</p>
-          <div class="card-actions justify-end">
-            <button disabled={slots.length===0} class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button disabled={slots.length===0} class="btn btn-primary">Booked Appointment</button>
           </div>
         </div>
       </div>
